a31aa105dd3116cdabd469daf19b6bf08e85211b
funayamateppei <t.funayama0217@gmail.com>
2022-11-03 04:48:57 +0900

    new

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ bar.addEventListener('click', () => {
 
 
 // ------------------ localStorage ------------------------
+const storage = [];
+const array = [];
+const l = storage.length;
+
+// ボタンクリックで配列にpush→localStorageに入れる
+$('.btn button').on('click', () => {
+  const data = {
+    artist: $('#artist').val(),
+    song: $('#song').val()
+  }
+  storage.push(data);
+  const jsonData = JSON.stringify(storage);
+  localStorage.setItem('kye', jsonData);
+  // 検索履歴作成
+  let end = storage[storage.length - 1];
+  array.push('<p>' + end.artist + ' _ ' + end.song + '</p>');
+  $('.header').append(`<div class="box${storage.length}"></div>`);
+  $(`.header .box${storage.length -1}`).append(array[storage.length - 1]);
+  $(`.header .box${storage.length -1}`).append('<button class="send">send</button>');
+  $(`.header .box${storage.length -1}`).append('<button class="delete">delete</button>');
+})
+
+// デリート機能
+// $('.delete').on('click', () => {
+
+// })
+
+// リロードしたときの更新
+if (localStorage.getItem('kye')) {
+  const jsonData = localStorage.getItem('kye');
+  const Data = JSON.parse(jsonData);
+  for (let i = 0; i < Data.length; i++) {
+    storage.push(Data[i]);
+  }
+  // 検索履歴作成
+  for (let i = 0; i < storage.length; i++) {
+    array.push('<p>' + storage[i].artist + ' _ ' + storage[i].song + '</p>');
+    $('.header').append(`<div class="box${i}"></div>`);
+    $(`.header .box${i}`).append(array[i]);
+    $(`.header .box${i}`).append('<button class="send">send</button>');
+    $(`.header .box${i}`).append('<button class="delete">delete</button>');
+  }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# iTunes API
+
+##
+
+##
+
+## 参考にしたサイト
+- フリー素材
+  - https://www.pinterest.jp/
+- iTunes API
+  - https://fieldwalking.jp/blog/html5-itunes-api/
+- 配列の最後の値を取得
+  - https://1-notes.com/javascript-get-the-last-value-from-the-array/
+- jQueryで要素を作成
+  - https://www.delftstack.com/ja/howto/jquery/jquery-create-element/
