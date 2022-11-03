1742e41c2c796f7d420c5db79a317b94f439f5a4
funayamateppei <t.funayama0217@gmail.com>
2022-11-04 01:52:59 +0900

    new

--- index.js
+++ index.js
@@ -30,15 +30,13 @@ $('.btn button').on('click', () => {
   const jsonData = JSON.stringify(storage);
   localStorage.setItem('kye', jsonData);
   // 検索履歴作成
-  let end = storage[storage.length - 1];
-  array.push('<p>' + end.artist + ' _ ' + end.song + '</p>');
-  $('.header').append(`<div class="box${storage.length - 1}"></div>`);
-  $(`.header .box${storage.length - 1}`).append(array[storage.length - 1]);
-  $(`.header .box${storage.length - 1}`).append(`<button class="send ${storage.length - 1}">send</button>`);
-  $(`.header .box${storage.length - 1}`).append(`<button class="delete num${storage.length - 1}">delete</button>`);
-
+  $('.header ol').remove()
+  array.push('<li>' + storage[storage.length - 1].artist + ' _ ' + storage[storage.length - 1].song + '</li>');
+  $('.header').append(`<ol></ol>`);
+  storage.map((element, index) => {
+    $(`ol`).append(array[index]);
+  })
   search();
-  console.log()
 })
 
 // リロードしたときの更新
@@ -48,67 +46,50 @@ if (localStorage.getItem('kye')) {
   for (let i = 0; i < Data.length; i++) {
     storage.push(Data[i]);
   }
-
-  const del = (index) => {
-    // クリックされたnumber番目の配列の中身を削除し、localStorageを上書き
-    console.log('hoge')
-    storage.splice(index, 1);
-    const jsonData = JSON.stringify(storage);
-    localStorage.setItem('kye', jsonData);
-    // すでに表示してある検索履歴を全部削除
-    $('.header div').remove();
-    // 削除後の配列を使って検索履歴を再作成
-    for (let i = 0; i < storage.length; i++) {
-      array.push('<p>' + storage[i].artist + ' _ ' + storage[i].song + '</p>');
-      // $('.header').append(`<div class="box${i}"></div>`);
-      // $(`.header .box${i}`).append(array[i]);
-      // $(`.header .box${i}`).append(`<button class="send num${i}">send</button>`);
-      // $(`.header .box${i}`).append(`<button class="delete num${i}">delete</button>`);
-    }
-
-    storage.map((value, index) => {
-      $('.header').append(`<div class="box${index}"></div>`);
-      $(`.box${index}`).append(array[index]);
-      $(`.box${index}`).append(`<button class="send">send</button>`);
-      $(`.box${index}`).append(`<button class="delete" onclick="${del(index)}">delete</button>`);
-    })
-  }
-
   // 検索履歴作成
   for (let i = 0; i < storage.length; i++) {
-    array.push('<p>' + storage[i].artist + ' _ ' + storage[i].song + '</p>');
-    // $('.header').append(`<div class="box${i}"></div>`);
-    // $(`.header .box${i}`).append(array[i]);
-    // $(`.header .box${i}`).append(`<button class="send num${i}">send</button>`);
-    // $(`.header .box${i}`).append(`<button class="delete num${i}">delete</button>`);
+    array.push('<li>' + storage[i].artist + ' _ ' + storage[i].song + '</li>');
   }
-  storage.map((value, index) => {
-    $('.header').append(`<div class="box${index}"></div>`);
-    $(`.header .box${index}`).append(array[index]);
-    $(`.header .box${index}`).append(`<button class="send">send</button>`);
-    $(`.header .box${index}`).append(`<button class="delete" onclick="${del(index)}">delete</button>`);
+  $('.header').append(`<ol></ol>`);
+  storage.map((element, index) => {
+    $(`ol`).append(array[index]);
   })
 }
 
 
-// デリート機能
-// 動的に作られたdivタグの連番をとれば変数に入れて配列を削除して
-// localStorage、検索履歴を更新できる
-// 動的に作った要素に対するクリックイベントの書き方では、クリックイベントは
-//  .headerに設定されている→index($(this))でクリックされた要素を持ってこれない
-$('.header').on('click', '.delete', () => {
-  // let number = $(this).index();
-  console.log(number);
 
+
+// デリート機能
+$('.delete').on('click', () => {
+  // 入力されている番号のデータを削除
+  let number = $('#number').val() - 1;
+  console.log(storage);
+  storage.splice(number, 1);
+  // 入力されたnumber番目の配列の中身を削除し、localStorageを上書き
+  const jsonData = JSON.stringify(storage);
+  localStorage.setItem('kye', jsonData);
+  // すでに表示してある検索履歴を全部削除
+  $('.header ol').remove();
+  console.log(array);
+  // リスト表示する配列を空にする
+  array.length = 0;
+  // 削除後の配列を使って検索履歴を再作成
+  for (let i = 0; i < storage.length; i++) {
+    array.push('<li>' + storage[i].artist + ' _ ' + storage[i].song + '</li>');
+  }
+  $('.header').append(`<ol></ol>`);
+  storage.map((element, index) => {
+    $(`ol`).append(array[index]);
+  })
 })
 
-// 検索履歴を使ったform上書き機能
-$('.header').on('click', '.send', () => {
-  let number = $('.header div').index($(this));
-  console.log(number);
 
-  // $('#artist').val(storage[number].artist);
-  // $('#song').val(storage[number].song);
+// 検索履歴を使ったform上書き機能
+$('.send').on('click', () => {
+  let number = $('#number').val() - 1;
+  $('#artist').val(storage[number].artist);
+  $('#song').val(storage[number].song);
+  search();
 })
 
 // --------------------- API -------------------------
@@ -156,7 +137,6 @@ const showData = (json) => {
     html += `<img src="${result.artworkUrl100}">`;
     html += '<p>Artist : ' + result.artistName + '</p>';
     html += '<audio src="' + result.previewUrl + '" controls />';
-    // html += '<button>ライブラリへ保存</button>';
     $(`.api .item${i}`).append(html);
   }
 }
@@ -165,10 +145,3 @@ const showData = (json) => {
 const crearTable = () => {
   $('.api div').remove();
 }
-
-
-// 検索する
-// search({
-//   term: 'きゃりーぱみゅぱみゅ',
-//   limit: 30
-// });
