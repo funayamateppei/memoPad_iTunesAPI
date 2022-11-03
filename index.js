776e8860c334f10aef9cd3c6daed2387ab03bd4e
funayamateppei <t.funayama0217@gmail.com>
2022-11-03 19:51:25 +0900

    add

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const array = [];
 
 // ボタンクリックで配列にpush→localStorageに入れる
 $('.btn button').on('click', () => {
+  if ($('#artist').val() === "" && $('#song').val() === "") {
+    alert("ArtistかSongを入力してください");
+    return;
+  }
   const data = {
     artist: $('#artist').val(),
     song: $('#song').val()
@@ -28,16 +32,14 @@ $('.btn button').on('click', () => {
   // 検索履歴作成
   let end = storage[storage.length - 1];
   array.push('<p>' + end.artist + ' _ ' + end.song + '</p>');
-  $('.header').append(`<div class="box${storage.length}"></div>`);
-  $(`.header .box${storage.length -1}`).append(array[storage.length - 1]);
-  $(`.header .box${storage.length -1}`).append('<button class="send">send</button>');
-  $(`.header .box${storage.length -1}`).append('<button class="delete">delete</button>');
+  $('.header').append(`<div class="box${storage.length - 1}"></div>`);
+  $(`.header .box${storage.length - 1}`).append(array[storage.length - 1]);
+  $(`.header .box${storage.length - 1}`).append(`<button class="send ${storage.length - 1}">send</button>`);
+  $(`.header .box${storage.length - 1}`).append(`<button class="delete num${storage.length - 1}">delete</button>`);
+
+  search();
+  console.log()
 })
-
-// デリート機能
-// $('.delete').on('click', () => {
-
-// })
 
 // リロードしたときの更新
 if (localStorage.getItem('kye')) {
@@ -51,7 +53,80 @@ if (localStorage.getItem('kye')) {
     array.push('<p>' + storage[i].artist + ' _ ' + storage[i].song + '</p>');
     $('.header').append(`<div class="box${i}"></div>`);
     $(`.header .box${i}`).append(array[i]);
-    $(`.header .box${i}`).append('<button class="send">send</button>');
-    $(`.header .box${i}`).append('<button class="delete">delete</button>');
+    $(`.header .box${i}`).append(`<button class="send num${i}">send</button>`);
+    $(`.header .box${i}`).append(`<button class="delete num${i}">delete</button>`);
   }
 }
+
+// デリート機能
+// 動的に作られたdivタグの連番をとれば変数に入れて配列、localStorageを削除できる
+// ↑まだコードは書いていない
+// クリックイベントは<div class=header></div>内のdivタグに設定されている
+//    →index($(this))でクリックされた要素を持ってこれない
+$('.header div').on('click', '.delete', () => {
+  let number = $('.header div').index($(this));
+  console.log(number)
+  console.log('hoge');
+})
+
+// --------------------- API -------------------------
+const search = () => {
+  // 検索ワード
+  const keyword = $('#artist').val() + ' ' + $('#song').val();
+  // 検索条件のベースとなるもの
+  const params = {
+    lang: 'ja_jp',
+    entry: 'music',
+    media: 'music',
+    country: 'JP',
+    term: keyword,
+    limit: 21
+  };
+
+  // iTunes API コール
+  $.ajax({
+    url: 'https://itunes.apple.com/search',
+    method: 'GET',
+    data: params,
+    // dataTypeをjsonpにする必要があります
+    dataType: 'jsonp',
+
+    // 処理が成功したら、jsonが返却されます
+    success: (json) => {
+      showData(json);
+      console.log(json);
+    },
+
+    error: () => {
+      console.log('itunes api search error. 検索に失敗しました');
+    }
+  })
+}
+
+// 成功したときに出力
+// 取得したデータを表示する
+const showData = (json) => {
+  crearTable();
+  for (let i = 0, len = json.results.length; i < len; i++) {
+    let result = json.results[i];
+    let html = '<p>Title : ' + " " + result.trackName + '</p>';
+    $('.api').append(`<div class="item${i}"></div>`)
+    html += `<img src="${result.artworkUrl100}">`;
+    html += '<p>Artist : ' + result.artistName + '</p>';
+    html += '<audio src="' + result.previewUrl + '" controls />';
+    html += '<button>ライブラリへ保存</button>';
+    $(`.api .item${i}`).append(html);
+  }
+}
+
+// すでに表示してあるものを削除
+const crearTable = () => {
+  $('.api div').remove();
+}
+
+
+// 検索する
+// search({
+//   term: 'きゃりーぱみゅぱみゅ',
+//   limit: 30
+// });
--- readme.md
+++ readme.md
@@ -7,9 +7,23 @@
 ## 参考にしたサイト
 - フリー素材
   - https://www.pinterest.jp/
+
 - iTunes API
   - https://fieldwalking.jp/blog/html5-itunes-api/
+
 - 配列の最後の値を取得
   - https://1-notes.com/javascript-get-the-last-value-from-the-array/
+
 - jQueryで要素を作成
   - https://www.delftstack.com/ja/howto/jquery/jquery-create-element/
+
+- iTunes API
+  - https://is2-ssl.mzstatic.com/image/thumb/Music/a1/45/df/mzi.uiclaofk.jpg/100x100bb.jpg
+  - https://enjoy-programing.com/itunes-api%E3%81%AE%E5%88%86%E3%81%8B%E3%82%8A%E3%82%84%E3%81%99%E3%81%84%E4%BD%BF%E3%81%84%E6%96%B9%E8%A7%A3%E8%AA%AC/
+  - https://www.yoheim.net/blog.php?q=20130702
+
+- 3点リーダーで文字数を制限する css
+  - https://rilaks.jp/blog/ellipsis/
+
+- 動的に付与されたものにイベント処理を行う　注意点
+  - https://qiita.com/sansaisoba/items/7099e63c4badb3b51b74
